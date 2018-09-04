9d50822713161b407a3798adad64a33450e4e9ce
tonmcg <tonmcg+github@gmail.com>
2018-09-03 20:51:29 -0400

    added new cutom data connector project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # powercensus
-Power Query M Functions to Access United States Census Bureau API
+Power Query M Custom Connector to Access United States Census Bureau API
+
+To Do:
+- Change authentication from Anonymous to API key
+- Implement NavTable for each of the 310 tables
---
+++ src/PowerCensus/PowerCensus/obj/Debug/PowerCensus.m
@@ -0,0 +1,32 @@
+﻿section Census;
+
+[DataSource.Kind="Census", Publish="Census.Publish"]
+shared Census.ListApis = Value.ReplaceType(CensusCall, type function () as any);
+
+CensusCall = () =>
+let
+    Source = Json.Document(Web.Contents("https://api.census.gov/data.json"))[dataset],
+    ConvertedToTable = Table.FromList(Source, Splitter.SplitByNothing(), {"dataset"}, null, ExtraValues.Error),
+    ExpandedDataset = Table.ExpandRecordColumn(ConvertedToTable, "dataset", {"title", "c_dataset", "c_vintage", "distribution", "c_isTimeseries", "temporal", "description", "modified"}, {"title", "name", "vintage", "url", "isTimeseries", "temporal", "description", "modified"}),
+    ExtractedNames = Table.TransformColumns(ExpandedDataset, {"name", each Text.Combine(List.Transform(_, Text.From), "/"), type text}),
+    ExpandedDistributionObject = Table.ExpandListColumn(ExtractedNames, "url"),
+    ExpandedURL = Table.ExpandRecordColumn(ExpandedDistributionObject, "url", {"accessURL"}, {"url"}),
+    DefinedDataTypes = Table.TransformColumnTypes(ExpandedURL,{{"title", type text}, {"vintage", Int64.Type}, {"url", type text}, {"isTimeseries", type logical}, {"temporal", type text}, {"description", type text}, {"modified", type datetime}}),
+    SortedRows = Table.Sort(DefinedDataTypes,{{"name", Order.Ascending}, {"vintage", Order.Ascending}})
+in
+    SortedRows;
+
+// Data Source Kind description
+Census = [
+    Authentication = [
+        Anonymous = []
+    ],
+    Label = "Census API"
+];
+
+// Data Source UI publishing description
+Census.Publish = [
+    Beta = true,
+    Category = "Other",
+    ButtonText = { "Census API", "Census API" }
+];
